e4ba2fc3aeb7072c54d641fe8702f454c7ff6cbf
Teboho Mokgosi <62904711+SirTebz@users.noreply.github.com>
2023-04-10 14:28:03 +0200

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A simple Hello World program
+            Console.WriteLine("Hello World");
+
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
